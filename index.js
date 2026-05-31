94fbe01e194a43e42d7b6ea60272c8c6013ab821
Yosimar <edgemovmix@gmailcom>
2026-05-31 02:16:03 -0600

    Actualización v1

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-app.set('view engine', 'ejs');
+app.get('/nombres', (req, res) => {
 
-app.get('/', (req, res) => {
-    res.render('index');
+    console.log("Diego");
+    console.log("Juan");
+
+    res.send("Juan Y Diego");
+
+});
+
+app.get('/planetas', (req, res) => {
+
+    res.send("Marte y Jupiter");
+
 });
 
 app.listen(PORT, () => {
