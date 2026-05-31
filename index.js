f7b0d07143130bd2f478e21246d4361a0869b0e9
Yosimar <edgemovmix@gmailcom>
2026-05-31 03:49:07 -0600

    cambio1

--- index.js
+++ index.js
@@ -5,27 +5,11 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
-app.get('/planetas/nombres', (req, res) => {
-
-    const nombres = [
-        'García',
-        'Martínez',
-        'López',
-        'Hernández',
-        'Ramírez'
-    ];
-
-    const planetas = [
-        'Marte',
-        'Júpiter'
-    ];
-
-    res.render('index', {
-        nombres,
-        planetas
-    });
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
 });
+
